a671d6cc7479aa3a35281151896fb5670d99563a
Yong Choi <yong.s.choi2@gmail.com>
2023-02-17 12:24:19 -0600

    js with html lecture notes

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("This is my external js file");
+
+//alert("Have a good lunch");
+
+let submit = confirm("Are you sure you want to resubmit this form?");
+console.log(submit);
+
+let age = prompt("what is your age?");
+alert("wow, you are old. You're " + age);
